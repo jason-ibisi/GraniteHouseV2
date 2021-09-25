0bdfda3fbc8c20826588e4ec1c74a68db01ac1ac
jason-ibisi <jasonibisi@gmail.com>
2021-09-25 19:34:37 +0100

    Add Product model and migration

---
+++ GraniteHouseV2/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GraniteHouseV2.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        [Range(1, int.MaxValue)]
+        public double Price { get; set; }
+        public string Image { get; set; }
+        [Display(Name = "Category Type")]
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public virtual Category Category { get; set; }
+    }
+}
