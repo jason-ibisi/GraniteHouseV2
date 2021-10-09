75a79d308af29ab6221fe9c74a3815157b8d5d5d
jason-ibisi <jasonibisi@gmail.com>
2021-10-09 19:43:59 +0100

    Home: Add product to cart

--- GraniteHouseV2/Controllers/HomeController.cs
+++ GraniteHouseV2/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 ﻿using GraniteHouseV2.Data;
 using GraniteHouseV2.Models;
 using GraniteHouseV2.Models.ViewModels;
+using GraniteHouseV2.Utility;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 
@@ -39,6 +41,20 @@ namespace GraniteHouseV2.Controllers
                 ExistsInCart = false
             };
             return View(detailsVM);
+        }
+        
+        [HttpPost, ActionName("Details")]
+        public IActionResult DetailsPost(int id)
+        {
+            List<ShoppingCart> shoppingCartList = new List<ShoppingCart>();
+            if (HttpContext.Session.Get<IEnumerable<ShoppingCart>>(AppConstants.SessionCart) != null 
+                && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(AppConstants.SessionCart).Count() > 0)
+            {
+                shoppingCartList = HttpContext.Session.Get<List<ShoppingCart>>(AppConstants.SessionCart);
+            }
+            shoppingCartList.Add(new ShoppingCart { ProductId = id });
+            HttpContext.Session.Set(AppConstants.SessionCart, shoppingCartList);
+            return RedirectToAction(nameof(Index));
         }
 
         public IActionResult Privacy()
