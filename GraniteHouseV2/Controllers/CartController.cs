a6a84f2e0280e9741d30047bbfb50374959e30e5
jason-ibisi <jasonibisi@gmail.com>
2021-12-11 17:56:18 +0100

    feat: Show list of items in Shopping Cart

---
+++ GraniteHouseV2/Controllers/CartController.cs
@@ -0,0 +1,37 @@
+﻿using GraniteHouseV2.Data;
+using GraniteHouseV2.Models;
+using GraniteHouseV2.Utility;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GraniteHouseV2.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CartController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            List<ShoppingCart> shoppingCartList = new List<ShoppingCart>();
+            
+            // check if session contains shopping cart session
+            if (HttpContext.Session.Get<IEnumerable<ShoppingCart>>(AppConstants.SessionCart) != null 
+                && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(AppConstants.SessionCart).Count() > 0)
+            {
+                shoppingCartList = HttpContext.Session.Get<IEnumerable<ShoppingCart>>(AppConstants.SessionCart).ToList();
+            }
+
+            // get list of product ids in cart
+            List<int> productInCart = shoppingCartList.Select(i => i.ProductId).ToList();
+            IEnumerable<Product> productsList = _db.Product.Where(p => productInCart.Contains(p.ProductId));
+
+            return View(productsList);
+        }
+    }
+}
