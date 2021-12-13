aea0b913373c3f7259d4bd0931d44030ed78d7ff
jason-ibisi <jasonibisi@gmail.com>
2021-12-13 21:49:36 +0100

    feat: Show Shopping Cart summary feat: Restrict unauthorized access to Shopping Cart Summary page

--- GraniteHouseV2/Controllers/CartController.cs
+++ GraniteHouseV2/Controllers/CartController.cs
@@ -1,15 +1,22 @@
 ﻿using GraniteHouseV2.Data;
 using GraniteHouseV2.Models;
+using GraniteHouseV2.Models.ViewModels;
 using GraniteHouseV2.Utility;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 
 namespace GraniteHouseV2.Controllers
 {
+    [Authorize]
     public class CartController : Controller
     {
         private readonly ApplicationDbContext _db;
+
+        [BindProperty]
+        public ProductUserVM ProductUserVM { get; set; }
 
         public CartController(ApplicationDbContext db)
         {
@@ -52,6 +59,43 @@ namespace GraniteHouseV2.Controllers
             HttpContext.Session.Set(AppConstants.SessionCart, shoppingCartList);
 
             return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Index")]
+        public IActionResult SubmitCartForSummary()
+        {
+            return RedirectToAction(nameof(ShoppingCartSummary));
+        }
+
+        public IActionResult ShoppingCartSummary()
+        {
+            // Get logged in user
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            // var userId = User.FindFirstValue(ClaimTypes.Name);
+
+            List<ShoppingCart> shoppingCartList = new List<ShoppingCart>();
+
+            // check if session contains shopping cart session
+            if (HttpContext.Session.Get<IEnumerable<ShoppingCart>>(AppConstants.SessionCart) != null
+                && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(AppConstants.SessionCart).Count() > 0)
+            {
+                shoppingCartList = HttpContext.Session.Get<IEnumerable<ShoppingCart>>(AppConstants.SessionCart).ToList();
+            }
+
+            // get list of product ids in cart
+            List<int> productInCart = shoppingCartList.Select(i => i.ProductId).ToList();
+            IEnumerable<Product> productsList = _db.Product.Where(p => productInCart.Contains(p.ProductId));
+
+            ProductUserVM = new ProductUserVM()
+            {
+                ApplicationUser = _db.ApplicationUser.FirstOrDefault(u => u.Id == claim.Value),
+                ProductList = productsList
+            };
+
+            return View(ProductUserVM);
         }
     }
 }
---
+++ GraniteHouseV2/Models/ViewModels/ProductUserVM.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace GraniteHouseV2.Models.ViewModels
+{
+    public class ProductUserVM
+    {
+        public ApplicationUser ApplicationUser { get; set; }
+        public IEnumerable<Product> ProductList { get; set; }
+
+        public ProductUserVM()
+        {
+            ProductList = new List<Product>();
+        }
+    }
+}
