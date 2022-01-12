8a5ff151a288ed9e163beb87a0208fd4e66bffef
jason-ibisi <jasonibisi@gmail.com>
2022-01-12 21:33:00 +0100

    refactor: add Category repository

---
+++ GraniteHouseV2_DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,26 @@
+﻿using GraniteHouseV2_DataAccess.Repository.IRepository;
+using GraniteHouseV2_Models;
+
+namespace GraniteHouseV2_DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Category category)
+        {
+            var categoryObjFromDb = base.FirstOrDefault(c => c.CategoryId == category.CategoryId);
+
+            if (categoryObjFromDb != null)
+            {
+                categoryObjFromDb.Name = category.Name;
+                categoryObjFromDb.DisplayOrder = category.DisplayOrder;
+            }
+        }
+    }
+}
---
+++ GraniteHouseV2_DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using GraniteHouseV2_Models;
+
+namespace GraniteHouseV2_DataAccess.Repository.IRepository
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        void Update(Category category);
+    }
+}
