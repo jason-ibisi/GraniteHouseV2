ae2e1c4bc7f17ebf46da2895b5d8a7a76980d9a7
jason-ibisi <jasonibisi@gmail.com>
2022-01-16 00:08:23 +0100

    refactor: add InquiryDetail and InquiryHeader models

---
+++ GraniteHouseV2_Models/InquiryHeader.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GraniteHouseV2_Models
+{
+    public class InquiryHeader
+    {
+        [Key]
+        public int InquiryId { get; set; }
+        public string ApplicationUserId { get; set; }
+        [ForeignKey("ApplicationUserId")]
+        public ApplicationUser ApplicationUser { get; set; }
+        public DateTime InquiryDate { get; set; }
+        [Required]
+        public string PhoneNumber { get; set; }
+        [Required]
+        public string FullName { get; set; }
+        [Required]
+        public string Email { get; set; }
+    }
+}
